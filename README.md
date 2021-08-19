255232e4f95b94d9dfc742b77d67c0424f470102
MahmoudSamy1452 <86854027+MahmoudSamy1452@users.noreply.github.com>
2021-08-19 18:28:45 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Project Description](#Project_Description)
+* [Installation](#Installation)
+* [Notes](#Notes)
+* [Usage](#Usage)
+* [References](#References)
+* [Credits](#Credits)
+
+## Project Description
+
+Given starting html and css files, the objective of this project is to turn the landing page to an animated, interactive and user-friendly  webpage. This is achieved by creating and attaching a javascript file to add dynamic features to this webpage. 
+
+Features added:
+
+* Building of navigation menu according to the number of sections on webpage.
+* Detecting and styling currently viewed section.
+* Smooth scrolling to a specific section upon clicking of corresponding link in the navigation menu.
+* Automatic hiding of navigation menu after 3 sec of inactivity.
+* Highlighting link of the active section in the navigation menu.
+
+All features added are compatible with all platforms running an up-to-date browser.  
+Some changes were made in html and css files for a better and effective design.
+
+## Installation
+
+You can clone the repository directly to your device to view the webpage or you can download the code from Github's Code button
+
+## Notes
+
+* The choice of not including an anchor tag inside each list item in the navigation menu was preferred because it needs to be overridden by
+ ```e.preventDefault();``` for the smooth transition between sections to take effect which is redundant.
+* Any function that was not found to be reusable was defined as a named anonymous function directly in its place of use.
+* Extra css rules were added to animate the motion of the navigation menu and the selection of a particular link. Enhancements to compatibility with small-screen devices by controlling the overflow of navigation menu items were implemented.
+
+## Usage
+
+This webpage can be used as a template for a landing page. (disclaimer: do not use without consulting Udacity)
+
+## References
+
+This sections includes all sources used as documentation to check the use of methods and properties used in the development of this project:
+
+* [W3School](https://www.w3schools.com/)
+* [MDN Web Docs](https://developer.mozilla.org/en-US/)
+* [Udacity](https://www.udacity.com/)
+
+## Credits
+
+This project was developed by *[Mahmoud Samy](https://github.com/MahmoudSamy1452)* as one of the project required to pass Front End Web Development Professional Nanodegree Program provided by Udacity
---
+++ js/app.js
@@ -0,0 +1,149 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+const navbarList = document.getElementById('navbar__list');
+const sections = document.getElementsByTagName('section');
+const noOfSections = sections.length;
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+// Return index of active of section of distance from top between -300 and 300
+function findActive(dists){
+	for (let i = 0; i < noOfSections; i++){
+		if (dists[i] <= 300 && dists[i] >= -300){
+			var activeIndex = i;
+		}	
+	}
+	return activeIndex;
+} 
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+function navBuild(){
+
+	// Fragment used to reduce reflow and repaint times for better performance
+	let fragment = document.createDocumentFragment();
+
+	// Used to index link id
+	let counter = 0;
+	
+	// Create link for each section and add it to fragment
+	for (section of sections){
+		counter++;
+		let navbarItem = document.createElement('li');
+		navbarItem.setAttribute('class','menu__link');
+		navbarItem.setAttribute('id',`link${counter}`);
+		navbarItem.textContent= section.getAttribute('data-nav');
+		fragment.appendChild(navbarItem);
+	}
+
+	navbarList.appendChild(fragment);
+}
+
+
+// Add class 'active' to section and its link when near top of viewport
+function activeSection(){
+
+	let distFromTop = [];
+
+	// Collecting distances between sections and the top of the viewport
+	for (section of sections){
+		distFromTop.push(section.getBoundingClientRect().top);
+	}
+
+	// Finding closest section to the top of the viewport
+	activeIndex = findActive(distFromTop);
+
+	// Removing active class from surrounding (above or below) sections if applied and applying it to the current active section if not applied
+	for (let indexSelection = activeIndex-1; indexSelection <= activeIndex + 1; indexSelection++){
+
+		// Skip indexSelection when out of index
+		if (indexSelection == -1 || indexSelection == noOfSections){
+			continue;
+		}
+		
+		// Set active section and link and unset other ones
+		let sectionSelection = document.querySelector(`#section${indexSelection+1}`);
+		let linkSelection = document.querySelector(`#link${indexSelection+1}`);
+		if (sectionSelection.classList.contains('your-active-class') && indexSelection != activeIndex){
+			sectionSelection.classList.remove('your-active-class');
+			linkSelection.classList.remove('your-active-link');
+		} else if (!sectionSelection.classList.contains('your-active-class') && indexSelection == activeIndex){
+			sectionSelection.classList.add('your-active-class');
+			linkSelection.classList.add('your-active-link');
+		}
+	}
+}
+
+// Scroll to anchor ID using scrollTO event
+function scrollToSection(event){
+
+	let link = event.target.getAttribute('id');
+
+	// Corresponding links and sections have a common index-like number at the end of their ids
+	let sectionNum = link[4];
+	var target = document.querySelector(`#section${sectionNum}`);
+
+	target.scrollIntoView({behavior: 'smooth'});
+}
+
+// Hide navigation when not in use
+function peek(){
+
+	navbarList.classList.add('your-active-nav');
+	let timeout = setTimeout(function hide(){
+		navbarList.classList.remove('your-active-nav');
+	},3000);
+
+	// Reset timeouts due to frequent sampling of scroll event
+	document.addEventListener('scroll',function reset(){
+		clearTimeout(timeout);
+		timeout;
+	});
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+document.addEventListener('DOMContentLoaded',navBuild);
+
+// Scroll to section on link click
+navbarList.addEventListener('click',scrollToSection);
+
+// Set sections as active
+document.addEventListener('DOMContentLoaded',activeSection);
+document.addEventListener('scroll',activeSection);
+
+// Display navigation menu after scroll
+document.addEventListener('DOMContentLoaded',peek);
+document.addEventListener('scroll',peek);
